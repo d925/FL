1c8bb919de396c7295453f0528f62c623f68d746
al22018 <al22018@shibaura-it.ac.jp>
2025-06-11 13:26:24 +0900

    proxに変更

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ fl.server.start_server(
     server_address="localhost:8080",
     config=ServerConfig(num_rounds=num_rounds),
     strategy=fl.server.strategy.FedAvg(
-        fraction_fit=0.1,
+        fraction_fit=0.5,
         proximal_mu=0.01,
     )
 )
