c1ec671054d5ef2e17137aac6bdca1895d71084d
al22018 <al22018@shibaura-it.ac.jp>
2025-06-11 17:16:23 +0900

    見やすく

--- server.py
+++ server.py
@@ -18,20 +18,16 @@ def save_metrics_to_json(round_number, accuracy, loss):
     with open(RESULTS_PATH, "w") as f:
         json.dump(data, f, indent=2)
 
-def aggregate_metrics(results):
-    global current_round
-    accuracies = [r.metrics["accuracy"] * r.num_examples for _, r in results]
-    losses = [r.loss * r.num_examples for _, r in results]
-    num_examples = sum(r.num_examples for _, r in results)
+def aggregate_metrics(results, server_round):
+    accuracies = [r["accuracy"] * r["num_examples"] for r in results]
+    losses = [r["loss"] * r["num_examples"] for r in results]
+    num_examples = sum(r["num_examples"] for r in results)
 
     avg_accuracy = sum(accuracies) / num_examples
     avg_loss = sum(losses) / num_examples
 
-    print(f"\n[Server] Round {current_round} summary → Accuracy: {avg_accuracy:.4f}, Loss: {avg_loss:.4f}\n")
-
-    save_metrics_to_json(current_round, avg_accuracy, avg_loss)
-
-    current_round += 1  # 次のラウンドへ
+    print(f"\n[Server] Round summary → Accuracy: {avg_accuracy:.4f}, Loss: {avg_loss:.4f}\n")
+    save_metrics_to_json(server_round, avg_accuracy, avg_loss)
 
     return {"accuracy": avg_accuracy, "loss": avg_loss}
 
