f7097c0a0bc6ffe94f196aca0b7549b0ccee0ef0
al22018 <al22018@shibaura-it.ac.jp>
2025-06-13 16:33:18 +0900

    GPU使用

--- run_clients.py
+++ run_clients.py
@@ -12,7 +12,7 @@ processes = []
 for client_id in range(num_clients):
     gpu_id = client_id % torch.cuda.device_count()  # GPU数に応じて割り振り
     env = dict(os.environ, CUDA_VISIBLE_DEVICES=str(gpu_id))
-    proc = subprocess.Popen(["python", "client.py", str(client_id)])
+    proc = subprocess.Popen(["python", "client.py", str(client_id)], env=env)
     processes.append(proc)
 
 for proc in processes:
