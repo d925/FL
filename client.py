af6b93670079e3d4f3a4ad2178dcafacbfbb43bd
al22018 <al22018@shibaura-it.ac.jp>
2025-06-16 15:11:10 +0900

    IID調整

--- client.py
+++ client.py
@@ -39,7 +39,7 @@ class FLClient(fl.client.NumPyClient):
         global_params = [p.clone().detach() for p in self.model.parameters()]
 
         self.model.train()
-        mu = config.get("proximal_mu", 0.1)
+        mu = config.get("proximal_mu", 0)
 
         for _ in range(5):
             for data, target in self.trainloader:
