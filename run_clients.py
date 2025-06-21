8794ca187dd8431ad55a5cc813758a49a47dd269
al22018 <al22018@shibaura-it.ac.jp>
2025-06-21 18:09:57 +0900

    クラスタ

--- run_clients.py
+++ run_clients.py
@@ -1,18 +1,30 @@
 # main.py
 import subprocess
-from config import num_clients
-from utils import generate_and_save_dirichlet_partitioned_data
 import torch
 import os
+import time
+from config import num_clients
+from utils import generate_and_save_dirichlet_partitioned_data
 
-
+# データパーティションを生成
 generate_and_save_dirichlet_partitioned_data(num_clients)
-processes = []
-for client_id in range(num_clients):
-    gpu_id = client_id % torch.cuda.device_count()  # GPU数に応じて割り振り
-    env = dict(os.environ, CUDA_VISIBLE_DEVICES=str(gpu_id))
-    proc = subprocess.Popen(["python", "client.py", str(client_id)], env=env)
-    processes.append(proc)
 
-for proc in processes:
+# サーバープロセスを起動
+server_proc = subprocess.Popen(["python", "server.py"])
+time.sleep(5)  # サーバー起動を待機
+
+# クライアントプロセスを起動
+client_procs = []
+for client_id in range(num_clients):
+    gpu_id = client_id % torch.cuda.device_count()
+    client_env = os.environ.copy()
+    client_env["CUDA_VISIBLE_DEVICES"] = str(gpu_id)
+    proc = subprocess.Popen(["python", "client.py", str(client_id)], env=client_env)
+    client_procs.append(proc)
+
+# クライアントの終了を待機
+for proc in client_procs:
     proc.wait()
+
+# サーバーの終了を待機
+server_proc.wait()
