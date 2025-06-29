a4ba5aa297fa2d43178cff63b51a6d668a097ee5
al22018 <al22018@shibaura-it.ac.jp>
2025-06-29 23:21:21 +0900

    シミュレーション

--- model.py
+++ model.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 class CNN(nn.Module):
-    def __init__(self, num_classes):
+    def __init__(self, num_classes: int = 38):
         super(CNN, self).__init__()
 
         # 畳み込み層ブロック1
