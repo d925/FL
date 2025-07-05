6610b369040790c74380667f829aee9a745ae5a9
al22018 <al22018@shibaura-it.ac.jp>
2025-07-05 19:01:47 +0900

    実験

--- model.py
+++ model.py
@@ -7,7 +7,7 @@ from torchvision.models import mobilenet_v3_large
 class MobileNetV3Classifier(nn.Module):
     def __init__(self, num_classes: int = 38):
         super(MobileNetV3Classifier, self).__init__()
-        base_model = mobilenet_v3_large(pretrained=False)  # 事前学習ありにしておくと性能向上
+        base_model = mobilenet_v3_large(pretrained=True)  # 事前学習ありにしておくと性能向上
         self.feature_extractor = base_model.features
         self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
         self.classifier = nn.Sequential(
