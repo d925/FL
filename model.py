73d339029c5c9d1f4662d4796c0ef73e61661a93
al22018 <al22018@shibaura-it.ac.jp>
2025-07-05 21:27:07 +0900

    実験

--- model.py
+++ model.py
@@ -6,7 +6,7 @@ from torchvision.models import resnet50, ResNet50_Weights
 class ResNet50Classifier(nn.Module):
     def __init__(self, num_classes: int = 38):
         super(ResNet50Classifier, self).__init__()
-        base_model = resnet50(weights=ResNet50_Weights.DEFAULT)  # ImageNetでの事前学習あり
+        base_model = resnet50(weights=None)  # ImageNetでの事前学習あり
 
         # 最終の全結合層（fc）を置き換える
         in_features = base_model.fc.in_features
