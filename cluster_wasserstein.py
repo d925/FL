9ef3aada8cbefeb0071e3c557a3ed2daeaf2273b
d925 <s.haruki20040218@gmail.com>
2025-11-18 22:01:47 +0900

    修正

--- cluster_wasserstein.py
+++ cluster_wasserstein.py
@@ -30,7 +30,7 @@ params = {
     "method": "distance",
     "cluster_method": "spectral",
     "k_range": list(range(3, 10)),
-    "alpha_grid": [0.3,0.4,0.5,0.6],
+    "alpha_grid": [0.3,0.4,0.5,0.6,0.7],
     "use_mds_for_visual": True,
     "mds_dim": 2,
     "random_state": 7,
