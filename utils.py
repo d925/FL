a5bb5e8cb8aa001c8e0a57f44e440453a044b6d9
d925 <s.haruki20040218@gmail.com>
2025-11-21 19:14:55 +0900

    seed1

--- utils.py
+++ utils.py
@@ -11,7 +11,7 @@ import random
 
 LABEL_ASSIGN_PATH = "label_assignments.json"
 DATA_DIR = "./Plant_leave_diseases_dataset_with_augmentation"
-PROCESSED_DATA_DIR = "./processed_dataset_0.1alpha_0"
+PROCESSED_DATA_DIR = "./processed_dataset_0.1alpha_1"
 
 
 def generate_and_save_dirichlet_partitioned_data(
@@ -31,7 +31,7 @@ def generate_and_save_dirichlet_partitioned_data(
     # =========================================================
     # 乱数シード固定
     # =========================================================
-    SEED = 0
+    SEED = 1
     random.seed(SEED)
     np.random.seed(SEED)
     torch.manual_seed(SEED)
